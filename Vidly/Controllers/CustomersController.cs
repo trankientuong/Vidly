2e6196d1c817eac7dfc3c5c4d0234adfae12f5d4
Trần Kiến Tường <55739577+trankientuong@users.noreply.github.com>
2022-02-27 13:09:14 +0700

    ActionResults, Action Parameter, Convention-based Routing, Attribute Routing, Razor Views and Exercise

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>()
+            {
+                new Customer{Id = 1, Name = "Tuong"},
+                new Customer{Id = 2, Name = "Phong"},
+                new Customer{Id = 3, Name = "Minh"},
+                new Customer{Id = 4, Name = "Bao"}
+            };
+        }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -17,7 +18,66 @@ namespace Vidly.Controllers
                 Id = 1,
                 Name = "Shrek!"
             };
-            return View(movie);
+
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer{Id = 1, Name = "Tuong"},
+                new Customer{Id = 2, Name = "Phong"},
+                new Customer{Id = 3, Name = "Minh"},
+                new Customer{Id = 4, Name = "Bao"}
+            };
+
+            //ViewData["Movie"] = movie;
+            //ViewBag.Movie = movie;
+            //var viewResult = new ViewResult();
+            //viewResult.ViewData.Model = movie;
+            //return viewResult;
+
+            RandomMovieViewModel randomMovieView = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(randomMovieView);
+
+
+
+
+            //return Content("Hello World");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home",new {page = 1, sortBy = "name"});
         }
+
+        public ActionResult Edit(int id )
+        {
+            return Content($"id= {id}");
+        }
+
+
+        // movies
+        public ActionResult Index(/*int? pageIndex, string sortBy*/)
+        {
+            //pageIndex = pageIndex ?? 1;
+            //if (string.IsNullOrWhiteSpace(sortBy))
+            //{
+            //    sortBy = "Name";
+            //}
+            //return Content($"pageIndex={pageIndex}-sortBy={sortBy}");
+
+            var movies = new List<Movie>(){
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie { Id = 2, Name = "Wall-e"}
+            };
+            return View(movies);
+
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+            return Content($"{year} / {month}");
+        }      
     }
 }
