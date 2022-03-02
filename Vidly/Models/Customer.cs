77c004b256df7a013865224b510b037ed6a9273c
Trần Kiến Tường <55739577+trankientuong@users.noreply.github.com>
2022-03-02 20:51:14 +0700

    Section4-Building Forms

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -13,7 +13,9 @@ namespace Vidly.Models
         [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubcribedToNewsletter { get; set; }
+        [Display(Name = "Date of Birth")]
         public DateTime Birthday { get; set; }
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
         public MembershipType MembershipType { get; set; }
     }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -11,12 +11,15 @@ namespace Vidly.Models
         public int Id { get; set; }
         public string Name { get; set; }
         [Required]
+        [Display(Name="Release Date")]
         public DateTime ReleaseDate { get; set; }
         [Required]
         public DateTime DateAdded { get; set; }
         [Required]
+        [Display(Name="Number In Stock")]
         public int NumberInStock { get; set; }
         [Required]
+        [Display(Name= "Genre")]
         public int GenreId { get; set; }
         public Genre Genre { get; set; }
 
